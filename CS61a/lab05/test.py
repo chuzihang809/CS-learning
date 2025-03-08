726a4ad6e4a32147e9ad15df0301816b1d3bdf3e
im not real <u202410239@hust.edu.cn>
2025-03-08 21:30:07 +0800

    create HW05

--- CS61a/lab05/test.py
+++ CS61a/lab05/test.py
@@ -1,4 +1,4 @@
-list = range(10)
-f = lambda x: x >= 5
-t = filter(f, list)
-print(next(t))
+def prefixes(s):
+    if s:
+        yield from prefixes(s[:-1])  # 递归调用，处理前缀（去掉最后一个字符）
+        yield s  # 生成当前字符串
